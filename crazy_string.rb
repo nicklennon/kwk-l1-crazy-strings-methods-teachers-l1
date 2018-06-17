766e619761a33647f9e09b9a28547aeb86207ffa
nicklennon <nicklennon.53@gmail.com>
2018-06-17 16:42:27 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,10 @@
-# write your method here
+# write your method herehttps://learn.co/tracks/kode-with-klossy-level-1/ruby/methods/crazy-strings-lab#
 
 def crazy_strings(a,b)
-
+  a.reverse.upcase + " " + b.swapcase.gsub("s","z").gsub("S","Z")
 end
+
+# Test Cases 
+# puts crazy_strings("Hello","Friends")
+# puts crazy_strings("APPLE", "")
+# puts crazy_strings("AA", "bb")
